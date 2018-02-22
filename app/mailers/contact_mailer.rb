403a3aeeb9b895278f26d9115eaf7eabb7a813e2
lkorsman <lukekorsman@gmail.com>
2018-02-22 18:36:43 +0000

    Reverted contact_mailer from email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,7 +6,7 @@ class ContactMailer < ActionMailer::Base
     @email = email
     @body = body
     
-    mail(from: name, subject: 'Contact Form Message')
+    mail(from: email, subject: 'Contact Form Message')
   end
   
 end
