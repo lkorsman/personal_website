19cbe6d7b2497cd8e0be9ca732b818366f469d66
lkorsman <lukekorsman@gmail.com>
2018-02-22 20:05:51 +0000

    Updated 'from' in contact_mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,7 +6,7 @@ class ContactMailer < ActionMailer::Base
     @email = email
     @body = body
     
-    mail(from: email, subject: 'Contact Form Message')
+    mail(from: 'lukekorsman@gmail.com', subject: 'Contact Form Message')
   end
   
 end
