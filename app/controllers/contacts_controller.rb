8e9530f585121fb5a0b4d0cf6765f2046e671505
lkorsman <lukekorsman@gmail.com>
2018-02-22 16:40:30 +0000

    Updated contact comments and environment.rb file

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,23 +1,39 @@
 class ContactsController < ApplicationController
+  
+  # GET request to /contact
+  # Show new contact form
   def new
     @contact = Contact.new
   end
   
+  # POST request /contacts
   def create
+    # Mass assignmemnt of form fields into contact object
     @contact = Contact.new(contact_params)
+    # Save contact object to database
     if @contact.save
+      # Store form fields via parameters, into variables
       name = params[:contact][:name]
       email = params[:contact][:email]
       body = params[:contact][:comments]
+      # Plug variables into Contact Mailer
+      # email method and send email
       ContactMailer.contact_email(name, email, body).deliver
+      # Store success message in flash hash
+      # and redirect to the new action
       flash[:success] = "Message sent."
       redirect_to new_contact_path
     else 
+      # If contact object doesn't save.
+      # store errors in flash hash,
+      # and redirect to new action
       flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to new_contact_path
     end
   end
   
+  # To collect data from form, we need to use
+  # strong parameters and whitelist the form fields
   private
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'mysterious-sands-23044.herokuapp.com',
+  :domain         => 'lukekorsman.tk',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
