042b3226dd81055a8ae9b2a314cdef6fc43b72b2
lkorsman <lukekorsman@gmail.com>
2018-02-22 16:56:25 +0000

    Updated environment.rb with new domain

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'lukekorsman.tk',
+  :domain         => 'mysterious-sands-23044.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
