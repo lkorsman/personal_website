2c9ea289d614332a0066410a3514d07bebd86950
lkorsman <lukekorsman@gmail.com>
2018-02-22 00:23:31 +0000

    Added experience page, updated homepage, about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
   
   def about
   end
+  
+  def experience
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'experience', to: 'pages#experience'
   resources :contacts, only: :create
   get 'contact-me', to: 'contacts#new', as: 'new_contact'
 end
