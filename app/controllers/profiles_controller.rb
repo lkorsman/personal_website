32691bea02f0015a76d4ab56af7c997704e546e3
lkorsman <lukekorsman@gmail.com>
2018-07-31 16:39:12 +0000

    Added authentication for users

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+   before_action :authenticate_user!
+   before_action :only_current_user
+   
    # GET to /users/:user_id/profile/new
    def new
       #render blank profile form
@@ -19,7 +22,12 @@ class ProfilesController < ApplicationController
      end
    end
    private
-   def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
-   end
+      def profile_params
+         params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      end
+      
+      def only_current_user
+         @user = User.find( params[:user_id] )
+         redirect_to(root_url) unless @user == current_user
+      end
 end
